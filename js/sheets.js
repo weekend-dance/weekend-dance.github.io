7690eda6902933243f59e0a3d44ee1e35b764872
Lunkle <d.ren.donny@gmail.com>
2023-07-09 11:36:50 -0400

    Update Acccess Token

--- js/sheets.js
+++ js/sheets.js
@@ -1,5 +1,5 @@
 const SHEET_ID = '1U59Jy4DXnfmhxF_1HxllLvZsmNWxuIEQwggySHpMwDs';
-const ACCESS_TOKEN = 'ya29.a0AbVbY6MVMKcrDPSJpdgRzFAQE2UvR6pcV-tYCAkv14xRzL5AnhoTxuqgQkrQZvvKyveeNrlkvSUWfWpyzJ_bVXmSw3fiju0cpzvSuNWsA2KNKn8z6HgdygoBQyMQvS9Qml9lKtqcjtK3WzVxcW_F5LqIHR_YaCgYKAeUSARASFQFWKvPlRXqs--9PhpTe_bEpXiT1iA0163';
+const ACCESS_TOKEN = 'ya29.a0AbVbY6O8axm8hJDCMGTOMI8DHlDxDczxSB6tFU4a9lPiJ_Od26BUdJbJhRcboNmcvkLLfOIDfudKrchXrC59PaGZoVxfCpQb1c_UbsrzFdgYmugmZNsfYpqOk4x8rkTF937UKjvS7htiLRlJj1XLuhKb9LF8sqcaCgYKAT0SARASFQFWKvPl9Gpgtwfoujn8y0BpzYeRcw0166';
 
 function addRow() {
     fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}:batchUpdate`, {
