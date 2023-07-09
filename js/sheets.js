e363b6e55fa2e082bfa9fad9bd9196e44b025366
Lunkle <d.ren.donny@gmail.com>
2023-07-09 10:00:06 -0400

    Sample Update Sheets

--- js/sheets.js
+++ js/sheets.js
@@ -1,3 +1,32 @@
+const SHEET_ID = '1U59Jy4DXnfmhxF_1HxllLvZsmNWxuIEQwggySHpMwDs';
+const ACCESS_TOKEN = 'ya29.a0AbVbY6MVMKcrDPSJpdgRzFAQE2UvR6pcV-tYCAkv14xRzL5AnhoTxuqgQkrQZvvKyveeNrlkvSUWfWpyzJ_bVXmSw3fiju0cpzvSuNWsA2KNKn8z6HgdygoBQyMQvS9Qml9lKtqcjtK3WzVxcW_F5LqIHR_YaCgYKAeUSARASFQFWKvPlRXqs--9PhpTe_bEpXiT1iA0163';
+
 function addRow() {
-    alert("hi");
+    fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}:batchUpdate`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${ACCESS_TOKEN}`,
+        },
+        body: JSON.stringify({
+            requests: [{
+                repeatCell: {
+                    range: {
+                        startColumnIndex: 0,
+                        endColumnIndex: 1,
+                        startRowIndex: 0,
+                        endRowIndex: 1,
+                        sheetId: 0
+                    },
+                    cell: {
+                        userEnteredValue: {
+                            "numberValue": 10
+                        },
+                    },
+                    fields: "*"
+                }
+            }]
+
+        })
+    })
 }
